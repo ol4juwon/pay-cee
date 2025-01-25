1f741e339f32b6b128abc04f3c75616ce7c9b749
Ola Juwon <ol4juwon>
2025-01-25 02:34:45 +0100

    updated config

--- src/configs/app.js
+++ src/configs/app.js
@@ -1,10 +1,10 @@
 let REST_ENDPOINT
 let BASE_URL;
-REST_ENDPOINT = 'http://localhost:4002/api/v1'
+// REST_ENDPOINT = 'http://localhost:4002/api/v1'
 
-// REST_ENDPOINT = 'https://vend-payout.onrender.com/api/v1'
+REST_ENDPOINT = 'https://payment-test-2hyc.onrender.com/api/v1'
 
-BASE_URL = "http//localhost:3000"
+BASE_URL = "https://payment-alva.vercel.app"
 
 const Config = {
   isDev: process.env.NODE_ENV === 'development',
